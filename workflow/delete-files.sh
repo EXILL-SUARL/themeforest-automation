d7c3024e6b7b2cf71b86f65de23ba458c7aaa25b
exilvm <exilvm@disroot.org>
2022-09-06 13:48:19 +0100

    manual format

--- workflow/delete-files.sh
+++ workflow/delete-files.sh
@@ -8,15 +8,15 @@ import shutil
 ignore_file = sys.argv[1]
 
 if not os.path.exists(ignore_file):
-    print('The path specified does not exist')
-    sys.exit()
+  print('The path specified does not exist')
+  sys.exit()
 
 file = open(ignore_file, 'r')
 for line in file:
   if not line.isspace() and not line.startswith('#'):
-      for filePath in glob.glob(line.strip(), recursive=True):
-        try:
-          shutil.rmtree(filePath)
-        except:
-          print('Something went wrong.')
+    for filePath in glob.glob(line.strip(), recursive=True):
+      try:
+        shutil.rmtree(filePath)
+      except:
+        print('Something went wrong.')
 file.close()
--- workflow/md-to-pdf.sh
+++ workflow/md-to-pdf.sh
@@ -2,8 +2,8 @@
 
 # validate required args
 if [[ -z "${1}" || -z "${2}" ]]; then
-    printf '%s\n' "Invalid CLI arguments" >&2
-    exit 1;
+  printf '%s\n' "Invalid CLI arguments" >&2
+  exit 1;
 fi
 
 pandoc -t beamer -o $2 $1
