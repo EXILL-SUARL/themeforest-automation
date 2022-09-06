9c0f03ab997e211b4d125b198c38770d7b95abfa
exilvm <exilvm@disroot.org>
2022-09-06 13:46:17 +0100

    exit with an error code

--- workflow/batch-blur.sh
+++ workflow/batch-blur.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ -z "${1}" ]]; then
-    echo "Target directory is missing."
-    exit;
+  printf '%s\n' "Target directory is missing." >&2
+  exit 1;
 fi
 
 find $1 -iregex '.*\.\(jpg\|gif\|ico\|png\|jpeg\|webp\)$' -type f -exec convert -blur 0x20 {} {} \;
