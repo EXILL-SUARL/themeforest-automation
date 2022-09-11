fd5aee5c19788897747762cb1f5f7ecb3bc77697
exilvm <exilvm@disroot.org>
2022-09-11 04:45:21 +0100

    fix permission inconsistency between prod container and dev container

--- deps.sh
+++ deps.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Update OS packages
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 
 # Install ImageMagick
-apt install imagemagick -y
+sudo apt install imagemagick -y
 
 # install markdown-to-document
 npm i markdown-to-document -g
