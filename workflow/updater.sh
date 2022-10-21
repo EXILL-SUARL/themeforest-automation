eb47b4a9760830fdb0af2bb1b0ab86114e16bfa0
exilvm <exilvm@disroot.org>
2022-10-21 19:51:45 +0100

    Remove pip dependency

--- workflow/updater.sh
+++ workflow/updater.sh
@@ -5,8 +5,5 @@ set -e
 # Update OS packages
 sudo apt update && apt upgrade -y
 
-# Update PIP packages
-pip install -U python-slugify
-
 # Update NPM packages
 npm i markdown-to-document@"<1.0.0" -g
