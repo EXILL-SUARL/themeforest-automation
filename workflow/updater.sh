57e4be0d4bddf6a86c9fd7bb1be58b242eb8166a
exilvm <exilvm@disroot.org>
2022-10-21 19:59:03 +0100

    Add `python-slugify` dependency

--- workflow/updater.sh
+++ workflow/updater.sh
@@ -5,5 +5,8 @@ set -e
 # Update OS packages
 sudo apt update && apt upgrade -y
 
+# Update PIP packages
+pip install -U python-slugify
+
 # Update NPM packages
 npm i markdown-to-document@"<1.0.0" -g
