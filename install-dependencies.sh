38c646be115d5bece275f3521e4bba31ebfeb7d6
exilvm <exilvm@disroot.org>
2022-10-23 14:01:28 +0100

    Fix typo

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -15,7 +15,7 @@ npm i markdown-to-document@"<1.0.0" -g
 cargo install --git https://github.com/chevdor/tera-cli
 
 # Install LicenseFinder
-gem install licenseFinder
+gem install license_finder
 
 # Install packages
 sudo apt install sudo curl zip imagemagick -y
