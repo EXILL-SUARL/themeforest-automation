c93db294722733f161ea3d537d49c9560a36415b
exilvm <exilvm@disroot.org>
2022-10-23 13:37:08 +0100

    Fix comments

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-# update OS packages
+# Update OS packages
 sudo apt update && sudo apt upgrade -y
 
-# install python-slugify
-pip install python-slugify
+# Install python-slugify
+pip Install python-slugify
 
-# install markdown-to-document
+# Install markdown-to-document
 npm i markdown-to-document@"<1.0.0" -g
 
-# install Tera CLI
+# Install Tera CLI
 cargo install --git https://github.com/chevdor/tera-cli
 
-# install packages
+# Install packages
 sudo apt install sudo curl zip imagemagick -y
