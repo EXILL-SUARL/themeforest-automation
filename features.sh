24a2601b0165738d0bd5c49c5c0b892252e48100
exilvm <exilvm@disroot.org>
2022-10-23 13:44:28 +0100

    Add Ruby and LicenseFinder

--- features.sh
+++ features.sh
@@ -16,6 +16,13 @@ RUSTPUP_PATH=$HOME/.cargo/env
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source $RUSTPUP_PATH && echo "source $RUSTPUP_PATH" >> $HOME/.bashrc
 
+# Install Ruby
+RUBY_PATH=/usr/local/rvm/scripts/rvm
+
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+
+curl -sSL https://get.rvm.io | bash -s stable --ruby="3.0" && source $RUBY_PATH && echo "source $RUBY_PATH" >> $HOME/.bashrc
+
 # Install Python 3 and PIP
 apt install python3 python3-pip -y
 
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,5 +14,8 @@ npm i markdown-to-document@"<1.0.0" -g
 # Install Tera CLI
 cargo install --git https://github.com/chevdor/tera-cli
 
+# Install LicenseFinder
+gem install licenseFinder
+
 # Install packages
 sudo apt install sudo curl zip imagemagick -y
