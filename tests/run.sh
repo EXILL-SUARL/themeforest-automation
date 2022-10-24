6658f17792cbe6b5723e3c3a074c5518c6e54d77
exilvm <exilvm@disroot.org>
2022-10-24 23:28:51 +0100

    Fix test runner not working in CI

--- tests/run.sh
+++ tests/run.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-TESTS_DIR="./tests"
 ORIGINAL_PWD="$PWD"
 
+if [ "$PROD" == 'true' ]; then
+  TESTS_DIR="/tmp/tests"
+else
+  TESTS_DIR="$PWD/tests"
+fi
+
 for dir in `ls $TESTS_DIR`; do
-TEST_DIR=$PWD/$TESTS_DIR/$dir
-if [  -d $TEST_DIR ]; then
+TEST_DIR=$TESTS_DIR/$dir
+if [ -d $TEST_DIR ]; then
   cd $TEST_DIR
   ./run.sh
   cd $ORIGINAL_PWD
