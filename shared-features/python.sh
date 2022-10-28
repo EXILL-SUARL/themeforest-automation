00b53d5a4dfbed3aaa403538b3a909006a8d8773
exilvm <exilvm@disroot.org>
2022-10-28 17:10:00 +0100

    Make Pyenv binaries accessible from both environments

--- shared-features/python.sh
+++ shared-features/python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 apt install git build-essential gdb lcov pkg-config libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev lzma lzma-dev tk-dev uuid-dev zlib1g-dev xz-utils -y
-curl https://pyenv.run | bash
-$HOME/.pyenv/bin/pyenv install 3.11.0
+PYENV_ROOT=/pyenv
+curl https://pyenv.run | PYENV_ROOT=$PYENV_ROOT bash
+$PYENV_ROOT/bin/pyenv install 3.11.0
