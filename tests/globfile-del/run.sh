53b4e633ec4c4cff1c15f1bfb0f689fe473dbfa6
exilvm <exilvm@disroot.org>
2022-10-28 22:43:23 +0100

    globalfile-del: Add test

---
+++ tests/globfile-del/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+globfile-del.sh ./assets/.ignore
+
+COUNT=$(find ./assets | wc -l)
+
+if [ $COUNT != 6 ]; then
+  printf '%s\n' "Files count test for globfile-del.sh has failed." >&2
+  exit 1;
+fi
