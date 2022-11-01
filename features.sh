34932115ff145e178919914e1643abf0a0eb9a30
exilvm <exilvm@disroot.org>
2022-11-01 22:16:37 +0100

    Load environment variables file if it exists in CI mode

--- features.sh
+++ features.sh
@@ -18,7 +18,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $RUSTPUP_PATH
 
-echo "source $RUSTPUP_PATH" >> ~/.bashrc
+echo "source $RUSTPUP_PATH" >> ~/.load_env
 
 # Install Ruby
 RUBY_PATH=/usr/local/rvm/scripts/rvm
@@ -29,7 +29,7 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby="3.0"
 
 source $RUBY_PATH
 
-echo "source $RUBY_PATH" >> ~/.bashrc
+echo "source $RUBY_PATH" >> ~/.load_env
 
 # Install Python 3 and PIP (both os-provided/3.11)
 apt install python3 python3-pip -y
