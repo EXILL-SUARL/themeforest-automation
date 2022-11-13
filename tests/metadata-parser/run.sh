470d0e8f29f4086a340551dd7574e81004f8fb01
exilvm <exilvm@disroot.org>
2022-11-14 00:01:43 +0100

    Add support for absolute/relative paths

--- tests/metadata-parser/run.sh
+++ tests/metadata-parser/run.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-PARSED_METADATA=$(metadata-parser.sh item-one.json item-two.json item-three.json item-four.json item-two.json)
+PARSED_METADATA=$(metadata-parser.sh ./item-one.json ./item-two.json ./item-three.json ./sub-dir/item-four.json ./item-two.json)
+
+echo $PARSED_METADATA > hi
 
 if ! diff <(cat results.json | jq --sort-keys) <(echo $PARSED_METADATA | jq --sort-keys) ; then
   printf '%s\n' "Test for metadata-parser.sh has failed." >&2
--- workflow/metadata-parser.sh
+++ workflow/metadata-parser.sh
@@ -6,6 +6,7 @@ import sys
 import os
 import json
 from slugify import slugify
+from pathlib import Path
 
 if len(sys.argv) == 1:
   sys.exit(f'Please specify a path. \nusage: {sys.argv[0]} <target_json_files_seperated_by_space>')
@@ -14,13 +15,14 @@ sys.argv.pop(0)
 
 data = {}
 
-for file_path in sys.argv:
-  if not os.path.exists(file_path):
+for file in sys.argv:
+  file_path = os.path.abspath(file)
+  if not file_path:
     sys.exit(f'{file_path} does not exist')
   with open(file_path) as file:
     file_content = file.read()
     parsed_file = json.loads(file_content)
-    stripped_filename = slugify(file_path, entities=True, decimal=False, hexadecimal=False, max_length=0,
+    stripped_filename = slugify(os.path.relpath(file_path, os.getcwd()), entities=True, decimal=False, hexadecimal=False, max_length=0,
       word_boundary=False, separator='_', save_order=False, stopwords=(), regex_pattern=None, lowercase=True, replacements=())
     data[stripped_filename] = parsed_file
 
